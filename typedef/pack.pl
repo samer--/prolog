8fbb24828df8ea0aaf22aa787d8070dee7b23651
Samer Abdallah <samer--@users.noreply.github.com>
2014-04-19 11:10:23 +0100

    Created separate typedef package.

---
+++ typedef/pack.pl
@@ -0,0 +1,7 @@
+name(memo).
+version('<VER>').
+author('Samer Abdallah','s.abdallah@ucl.ac.uk').
+title('Persistent memoisation of deterministic predicates').
+keywords([memoisation,persistency]).
+download('https://github.com/samer--/prolog/blob/master/memo/release/memo-<VER>.tgz?raw=true').
+
