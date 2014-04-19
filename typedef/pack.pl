fc05d96fde16d01008e4f74d3caa6dd1083d4ffd
Samer Abdallah <samer--@users.noreply.github.com>
2014-04-19 22:43:03 +0100

    Fix typedef download location.

--- typedef/pack.pl
+++ typedef/pack.pl
@@ -3,5 +3,5 @@ version('<VER>').
 author('Samer Abdallah','s.abdallah@ucl.ac.uk').
 title('Support for type definitions').
 keywords([types]).
-download('http://github.com/samer--/prolog/blob/master/typedef/release/typdef-<VER>.tgz?raw=true').
+download('http://github.com/samer--/prolog/blob/master/typedef/release/typedef-<VER>.tgz?raw=true').
 
