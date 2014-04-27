0c92cec9ce4fbfd7ef5554f2a614eca0de2d2450
Samer Abdallah <samer--@users.noreply.github.com>
2014-04-27 22:41:28 +0100

    ADDED: test.pl, updated release to 0.2.4

---
+++ callgraph/examples/test.pl
@@ -0,0 +1,25 @@
+:- use_module(library(callgraph)).
+
+/** 
+   Use these to test callgraph.
+*/
+
+
+%% test_pdf is det.
+%  Writes callgraph.pdf in the current directry.
+test_pdf :-
+	module_dotpdf(callgraph,[method(unflatten([fl(4),c(4)]))]).
+
+%% test_dot is det.
+%  Writes callgraph.dot in the current directry.
+test_dot :-
+	module_dot(callgraph,[]).
+
+%% test_xdot is det.
+%  Writes callgraph.dot in the current directry and then
+%  calls xdot to display it. Will bork if xdot not present.
+%  Pack gvterm includes xdot, but you should probably get the
+%  latest version from http://code.google.com/p/jrfonseca/wiki/XDot
+test_xdot :-
+	test_dot,
+   shell('xdot callgraph.dot').
--- callgraph/pack.pl
+++ callgraph/pack.pl
@@ -1,8 +1,8 @@
-name(callgraph).
+name(<PACKNAME>).
 version('<VER>').
 author('Samer Abdallah','s.abdallah@ucl.ac.uk').
 title('Predicate call graph visualisation').
 keywords([cross_referencing,graph,visualisation,dot]).
-download('https://github.com/samer--/prolog/raw/master/callgraph/release/callgraph-<VER>.tgz').
+download('https://github.com/samer--/prolog/raw/master/<PACKNAME>/release/<PACKNAME>-<VER>.tgz').
 requires(fileutils).
 
