99a700fd8a9b2c9c7bd684e770d04e0006b56b15
Samer Abdallah <samer--@users.noreply.github.com>
2014-04-27 13:48:54 +0100

    Switched download location to github.

--- callgraph/pack.pl
+++ callgraph/pack.pl
@@ -3,6 +3,6 @@ version('<VER>').
 author('Samer Abdallah','s.abdallah@ucl.ac.uk').
 title('Predicate call graph visualisation').
 keywords([cross_referencing,graph,visualisation,dot]).
-download('http://code.soundsoftware.ac.uk/projects/callgraph/repository/raw/release/callgraph-<VER>.tgz').
+download('https://github.com/samer--/prolog/raw/master/callgraph/release/callgraph-<VER>.tgz').
 requires(fileutils).
 
