c8b8c944187ba454a609e77ea4dbddacf9d542fe
Samer Abdallah <samer--@users.noreply.github.com>
2014-07-30 12:56:58 +0100

    Changed download address to https to http

--- memo/pack.pl
+++ memo/pack.pl
@@ -3,6 +3,6 @@ version('<VER>').
 author('Samer Abdallah','s.abdallah@ucl.ac.uk').
 title('Persistent memoisation of deterministic predicates').
 keywords([memoisation,persistency]).
-download('https://github.com/samer--/prolog/raw/master/memo/release/memo-*.tgz').
+download('http://github.com/samer--/prolog/raw/master/memo/release/memo-*.tgz').
 requires(typedef).
 
