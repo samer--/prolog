d06b81c86b7e530315818a67765577a9ff4d09aa
Samer Abdallah <samer--@users.noreply.github.com>
2014-11-27 22:16:17 +0000

    Moved plsmf out to own repository

--- plsmf/pack.pl
+++
@@ -1,6 +0,0 @@
-name(plsmf).
-version('0.2.0').
-author('Samer Abdallah','s.abdallah@ucl.ac.uk').
-title('Standard MIDI File reading').
-% download('https://github.com/samer--/prolog/raw/master/<PACKNAME>/release/<PACKNAME>-<VER>.tgz').
-
