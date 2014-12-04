168715ddb1186accb9a44f07d9e1d93a438366d1
Samer Abdallah <samer--@users.noreply.github.com>
2014-12-04 16:53:31 +0100

    Something funny wrong with old pack file..

--- sindice/pack.pl
+++ sindice/pack.pl
@@ -3,4 +3,3 @@ version('<VER>').
 author('Samer Abdallah','s.abdallah@ucl.ac.uk').
 title('Access to Sindice semantic web search engine').
 download('https://github.com/samer--/prolog/raw/master/<PACKNAME>/release/<PACKNAME>-<VER>.tgz').
-
