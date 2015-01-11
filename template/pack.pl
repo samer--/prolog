61c1b1f7f6a4831656b6425c876e670512bb4acc
Samer Abdallah <samer--@users.noreply.github.com>
2015-01-11 23:38:43 +0000

    Updated template Makefile to new scheme.

--- template/pack.pl
+++ template/pack.pl
@@ -1,6 +1,6 @@
 name(<PACKNAME>).
 version('<VER>').
 author('Samer Abdallah','s.abdallah@ucl.ac.uk').
-title('Predicate call graph visualisation').
+title('Title of new pack').
 download('https://github.com/samer--/prolog/raw/master/<PACKNAME>/release/<PACKNAME>-<VER>.tgz').
 
