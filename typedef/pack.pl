7233d521b39db2571cb56bb8cd03a67bc9dab5fa
Samer Abdallah <samer--@users.noreply.github.com>
2015-01-14 22:00:59 +0000

    Bah. maybe it will work this time.

--- typedef/pack.pl
+++ typedef/pack.pl
@@ -3,4 +3,5 @@ version('<VER>').
 author('Samer Abdallah','s.abdallah@ucl.ac.uk').
 title('Support for type definitions').
 keywords([types]).
-download('https://github.com/samer--/prolog/raw/master/typedef/release/typedef-*.tgz').
+download('https://github.com/samer--/prolog/raw/master/<PACKNAME>/release/<PACKNAME>-<VER>.tgz').
+
