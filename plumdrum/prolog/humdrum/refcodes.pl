b96307a644d4c0344ec44c96d19d42e11ebe9d03
Samer Abdallah <samer--@users.noreply.github.com>
2015-05-18 19:41:29 +0100

    Added LGPL

--- plumdrum/prolog/humdrum/refcodes.pl
+++ plumdrum/prolog/humdrum/refcodes.pl
@@ -1,3 +1,21 @@
+/* Part of plumdrum
+	Copyright 2012-2015 Samer Abdallah (Queen Mary University of London; UCL)
+	 
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Lesser General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public
+	License along with this library; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 :- module(refcodes,[ refcode/3 ]).
 /** <module> Humdrum reference code database
 
