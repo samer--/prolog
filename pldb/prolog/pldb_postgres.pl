da02710cf76f8d124606af6d62e1e556ac07374f
Samer Abdallah <samer--@users.noreply.github.com>
2015-06-09 17:45:02 +0100

    Fixed calls to float//1 DCG rule

--- pldb/prolog/pldb_postgres.pl
+++ pldb/prolog/pldb_postgres.pl
@@ -5,16 +5,16 @@
 
 :- multifile pldb:typed_value, pldb:type_decode/3.
 
-pldb:typed_value(T,V) --> {is_geometric(T)}, !, quote(geo(T,V)).
+pldb:typed_value(T,V) --> {is_geometric(T)}, !, "'",geo(T,V),"'".
 
 pldb:type_decode(F,X,P) :- is_geometric(F), !, atom_codes(X,C), phrase(geo(F,P),C,[]). 
 
-geo( point,   point(A,B))         --> paren((float(A), comma, float(B))).
+geo( point,   point(A,B))         --> paren((pldb:float(A), comma, pldb:float(B))).
 geo( lseg,    linesegment(P1,P2)) --> paren(( geo(point,P1), comma, geo(point,P2))).  
 geo( lseg,    line(P1,P2))        --> sqbr(((geo(point,P1), comma, geo(point,P2)))).
 geo( line,    line(P1,P2))        --> sqbr(((geo(point,P1), comma, geo(point,P2)))).
 geo( box,     box(P1,P2))         --> geo(point,P1), comma, geo(point,P2).
-geo( circle,  circle(P,R))        --> "<", geo(point,P), comma, float(R), ">". 
+geo( circle,  circle(P,R))        --> "<", geo(point,P), comma, pldb:float(R), ">".
 geo( path,    path(closed,PX))    --> paren(seqmap_with_sep(comma, geo(point), PX)).
 geo( path,    path(open,PX))      --> sqbr(seqmap_with_sep(comma, geo(point), PX)).
 geo( polygon, polygon(PX))        --> paren(seqmap_with_sep(comma, geo(point), PX)).
