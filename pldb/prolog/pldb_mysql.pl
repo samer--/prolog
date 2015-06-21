64edaab4e184523f2d9f54e06132eed044b5845d
Samer Abdallah <samer--@users.noreply.github.com>
2015-06-21 11:16:55 +0100

    Added datatypes.

--- pldb/prolog/pldb_mysql.pl
+++ pldb/prolog/pldb_mysql.pl
@@ -1,6 +1,10 @@
 :- module(pldb_mysql,[]).
 :- multifile pldb:type_class/2.
 
-type_class(tinyint,     numeric).
+type_class(tinyint,    numeric).
+type_class(smallint,   numeric).
+type_class(mediumint,  numeric).
+type_class(int,        numeric).
+type_class(bigint,     numeric).
 type_class('integer unsigned', numeric).
 type_class('tinyint unsigned', numeric).
