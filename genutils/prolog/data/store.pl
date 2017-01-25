3a23507ffc13d4701a7ede1d060a6f37f9b14ea9
samer-- <s.abdallah@ucl.ac.uk>
2017-01-25 14:48:04 +0000

    Add store_upd//3 and move env from plumdrum to genutils

--- genutils/prolog/data/store.pl
+++ genutils/prolog/data/store.pl
@@ -3,6 +3,7 @@
 	,	store_add//2
 	,	store_get//2
 	,	store_set//2
+	,	store_upd//3
 	,	store_apply//2
    ,  store_contains//2
 	]).
@@ -48,6 +49,13 @@ store_contains(Ref,  Term, store(N,T), store(N,T)) :- gen_assoc(Ref,T,Term).
 %  Update the term referred to by R to take the new values Term.
 store_set(Ref, Term, store(N,T1), store(N,T2)) :- get_assoc(Ref,T1,_,T2,Term).
 
+%% store_upd(+R:ref(A), X1:A, X2:A, +H1:store, -H2:store) is det.
+%  Unify X1 with the value of R in H1 and replace the value
+%  with X2 to get H2. The same effect can be achieved with store_apply//2
+%  but this saves a metacall.
+store_upd(Ref, N1, N2, store(N,T1), store(N,T2))  :- 
+	get_assoc(Ref,T1,N1,T2,N2).
+
 %% store_apply(+R:ref(A), Op:pred(A,A), +H1:store, -H2:store) is det.
 %  Apply binary predicate Op to the term in the store reference by R.
 %  The old value is replaced by the new value return by Op in its
@@ -62,6 +70,7 @@ store_new(store(0,A)) :- rb_empty(A).
 store_add(Term, N1, store(N1,T1), store(N2,T2)) :- rb_insert_new(T1,N1,Term,T2), succ(N1,N2).
 store_get(Ref,  Term, store(N,T), store(N,T))   :- rb_lookup(Ref,Term,T).
 store_set(Ref, Term, store(N,T1), store(N,T2))  :- rb_update(T1,Ref,Term,T2).
+store_upd(Ref, Term, Term2, store(N,T1), store(N,T2))  :- rb_update(T1,Ref,Term,Term2,T2).
 store_apply(Ref, Op, store(N,T1), store(N,T2))  :- rb_apply(T1,Ref,Op,T2).
 */
 
