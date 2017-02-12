fb50af7d9f5cce00efe0bfb02e681631835b5876
samer-- <s.abdallah@ucl.ac.uk>
2017-02-12 22:23:17 +0000

    Add variant probabistic tabling

---
+++ cclab/prolog/cclazy/ccvprob.pl
@@ -0,0 +1,95 @@
+:- module(ccvprob, [ run_ltree/2, dist/2, guard/1, fail_/0, cctabled/1
+                   , leaf_prob/4, get_tables/2]).
+
+/** <module> Probabilistic choice as a lazy search tree with recursive memoisation of multiple variants
+
+This is like ccprob, but instead of memoising binary predicates (Input -> Output), it
+handles predicates of any arity with separate tables for each calling pattern, like ccvmemo.
+*/
+
+:- use_module(library(clpr)).
+:- use_module(library(rbtrees)).
+:- use_module(library(typedef)).
+:- use_module(library(callutils), [mr/5]).
+:- use_module(library(data/pair), [snd/2]).
+:- use_module(library(delimcc), [pr_reset/3, pr_shift/2, p_shift/2]).
+:- use_module(library(ccstate), [app/1, upd/2, app/2, run_state/4, get/1]).
+:- use_module(library(lambda2)).
+:- use_module(library(apply_macros)).
+
+:- set_prolog_flag(back_quotes, symbol_char).
+
+:- type tree(A) ---> leaf(A); node(list(tree(A))).
+:- type ltree(A) ---> leaf(A); lnode(pred(list(ltree(A)))).
+:- type ltree(L,A) ---> leaf(A); lnode(L,pred(list(ltree(A)))). % with labelled nodes
+
+:- meta_predicate cctabled(0).
+cctabled(Head) :- pr_shift(prob, mem(Head,P)), app(expl, ccvprob:c(Head-v(P))). % weird context bug
+dist(Xs,X) :- pr_shift(prob, dist(Xs,P-X)), app(expl, c(@X-l(P))).
+c(X,[X|T],T).
+
+:- meta_predicate guard(0).
+guard(P) :- call(P) -> true; pr_shift(prob, fail).
+fail_ :- pr_shift(prob, fail).
+
+get_tables(Tabs1, Map) :- get(Tabs), rb_fold(mr(sanitize(Map),c), Tabs, Tabs1,[]).
+sanitize(Map, V-entry(Solns,_), V-Expls) :- rb_fold(mr(map_soln(Map),c), Solns, Expls,[]).
+
+map_soln(Map, Y-e(Prob,Expls), Y-e(Prob1,Expls1)) :-
+   memberchk(Prob-Prob1, Map),
+   maplist(maplist(pp(Map)), Expls, Expls1),
+   foldl(mr(expl_prob,add), Expls1, 0, Prob1).
+
+leaf_prob(M, S:-Ex, S:-Ex2, P) :- 
+   maplist(pp(M), Ex, Ex2),
+   expl_prob(Ex2, P).
+
+pp(_, X-l(P), X-P) :- !.
+pp(Map, X-v(P), X-P1) :- memberchk(P-P1, Map).
+expl_prob(Expl, P) :- foldl(mr(snd,mul),Expl,1,P). 
+add(X,Y,Z) :- {Z=X+Y}.
+mul(X,Y,Z) :- {Z=X*Y}.
+
+
+%% run_ltree(+P:pred, -T:ltree(list(any))) is det.
+:- meta_predicate run_ltree(0,-).
+run_ltree(Goal,Result) :- 
+   term_variables(Goal, Ans),
+   pr_reset(prob, \\leaf(Ans:-Expl)`run_state(expl, Goal, Expl, []), Result).
+
+dist(Xs,PX,K,lnode(dist(Xs), ccvprob:maplist(callprob(\\PX`K),Xs))).
+fail(_,lnode(fail,=([]))).
+
+mem(Head,P,K,Tree) :-
+   term_variables(Head,Y), 
+   head_to_variant(Head, Variant),
+   KPY = (\\P-Y`K),
+   upd(Tabs1, Tabs2),
+   (  rb_update(Tabs1, Variant, entry(Ys,Ks), entry(Ys,[KPY|Ks]), Tabs2)
+   -> Tree = lnode(cons(Variant,Ys), ccvprob:rb_fold(cons_expand(KPY),Ys,[]))
+   ;  rb_empty(EmptySet),
+      rb_insert_new(Tabs1, Variant, entry(EmptySet,[]), Tabs2),
+      run_state(expl, call(\\Y`Head, YNew), Expl, []),
+      app(tab_upd(Variant, entry(Ys,Ks), entry(Ys2,Ks))),
+      (  rb_insert_new(Ys, YNew, e(PY,[Expl]), Ys2) % PY will be the sum of the probabilities of explanations
+      -> Tree=lnode(prod(Variant,YNew), ccvprob:maplist(send_to_cont(PY-YNew),[KPY|Ks])), gensym(p,PY)
+      ;  rb_update(Ys, YNew, e(PY,Expls), e(PY,[Expl|Expls]), Ys2),
+         Tree=lnode(dup(Variant,YNew),=([]))
+      )
+   ).
+
+cons_expand(KPY,Y-e(P,_),Ts,[T|Ts]) :- callprob(KPY,P-Y,T).
+send_to_cont(PY,KPY,T) :- callprob(KPY,PY,T).
+callprob(KPY,PY,T)     :- pr_reset(prob, call(KPY,PY), T).
+tab_upd(K,V1,V2,T1,T2) :- rb_update(T1,K,V1,V2,T2).
+
+head_to_variant(Head, Variant) :-
+   copy_term_nat(Head, Variant),
+   numbervars(Variant, 0, _).
+
+% for printing annotated search trees
+user:portray(dist(Xs))   :- write('?'), maplist(\\F-X`X:S`format(string(S),'~2g',[F]), Xs, Ss), write(Ss).
+user:portray(cons(_:X,Ys)) :- rb_keys(Ys,Vals), write('C':X>Vals).
+user:portray(dup(_:X,Y)) :- write('D':X>Y).
+user:portray(prod(_:X,Y)) :- write('P':X>Y).
+
---
+++ cclab/prolog/cclazy/test_vprob.pl
@@ -0,0 +1,133 @@
+:- module(test_vprob, []).
+
+/** <module> Test variant memoisation with explanation graphs
+   Eg try this:
+   ==
+   ?- explore_ltree(pathl(a,K), print_and_dump).
+   ==
+*/
+:- use_module(library(clpr)).
+:- use_module(library(data/pair), [pair/3, snd/2]).
+:- use_module(library(ccstate),  [run_state/3]).
+:- use_module(ccvprob, [ leaf_prob/4, guard/1, dist/2, fail_/0, run_ltree/2, cctabled/1, get_tables/2]).
+:- use_module(library(ccmacros)).
+:- use_module(library(lambda1)).
+:- use_module(treeutils).
+
+
+% -------------- calling stateful nondeterminism ------
+
+explore_ltree(Goal, Explore) :-
+   rb_empty(Empty),
+   run_state(generate_explore(Goal, Explore), Empty, _).
+
+generate_explore(Goal, Explore) :-
+   run_ltree(Goal, Tree),
+   call(Explore, Tree).
+
+print_probs_and_dump(Ts1,T1,T) :-
+   convert_tree(T,T1),
+   get_tables(Ts1,Map),
+   maptree(apply_probs(Map), T1, T2),
+   maplist(print_variant,Ts1),
+   once(print_tree(T2)).
+
+solns_and_probs(T) :-
+   tree_yield(T, Ls, []),
+   get_tables(Ts1,Map),
+   maplist(leaf_prob(Map), Ls, Ls1, Ps1),
+   maplist(pair, Ls1, Ps1, LPs),
+   sumlist(Ps1, Tot),
+
+   maplist(print_variant,Ts1), nl,
+   maplist(pprintln(' '), LPs), nl,
+   pprintln('Total probability:', Tot).
+
+
+apply_probs(M,l(L1),l(L2)) :- !, leaf_prob(M,L1,L2,_).
+apply_probs(_,L,L).
+
+user:portray(X) :- float(X), !, format('~3g',[X]).
+
+print_variant(Var-Solns) :- pprintln('',Var), maplist(print_soln,Solns).
+print_soln(Y-e(Prob,Expls)) :- pprintln('  ',Y:Prob), maplist(pprintln('    '),Expls).
+pprintln(Pref,X) :- write(Pref), print(X), nl.
+
+:- module(test_vprob).
+
+% ---- test programs -----
+choose(Xs,X) :-
+   length(Xs,N),
+   P is 1/N,
+   maplist(pair(P),Xs,Dist),
+   dist(Dist,X).
+
+test(Z) :-
+   choose([1,2,3],X),
+   choose([a(X),b(X),c(X)],Y),
+   choose([Y-x, Y-y, Y-z],Z1),
+   choose([Z1-u, Z1-v, Z1-w],Z).
+
+link(a,X) :- choose([b,c],X).
+link(b,d).
+link(c,X) :- choose([d,m],X).
+link(d,X) :- choose([e,f],X).
+link(e,g).
+link(f,X) :- choose([g,k],X).
+link(g,h).
+link(h,X) :- choose([i,j],X).
+link(i,_) :- fail_.
+link(j,_) :- fail_.
+link(k,l).
+link(l,_) :- fail_.
+link(m,_) :- fail_.
+
+link(w,X) :- dist([0.75-x,0.25-y],X).
+link(x,X) :- dist([0.5-v,0.5-z],X).
+link(y,z).
+% link(z,X) :- dist([1-u],X).
+link(z,X) :- dist([0.5-t, 0.5-u],X).
+link(t,_) :- fail_.
+link(u,_) :- fail_.
+link(v,_) :- fail_.
+
+id --> [].
+
+:- cctabled sent//0.
+
+% left and right recursive grammar
+sent --> {choose([b,l,r],A)}, sentx(A).
+sentx(b) --> word.
+sentx(r) --> sent, out(not).
+sentx(l) --> out(really), sent.
+
+word --> {choose([cool,wicked],W)}, out(W).
+
+out(_,[],_) :- !, guard(false).
+out(W,[X|T],T) :- guard(W=X).
+
+% incomplete recursive form
+
+:- cctable pathr//0, pathl//0.
+
+pathr --> link, {choose(['B','R'],Q)}, maybe_rec(Q,pathr).
+pathl --> {dist([0.6-'B', 0.4-'R'],Q)}, maybe_rec(Q,pathl), link.
+
+bernoulli(P,X) :- NP is 1 - P, dist([NP-0,P-1],X).
+
+maybe_rec('B',_) --> [].
+maybe_rec('R',P) --> call(P).
+
+
+make_fib(Fib) :- Fib = cctabled(fib_inc(Fib)). % ha
+
+fib_inc(_,0,1) :- !.
+fib_inc(_,1,1) :- !.
+fib_inc(Fib,N,X) :-
+   succ(M,N), call(Fib,M,Y),
+   succ(L,M), call(Fib,L,Z),
+   X is Y+Z.
+
+test_fib(Fib,Goal) :-
+   make_fib(Fib),
+   call(Goal).
