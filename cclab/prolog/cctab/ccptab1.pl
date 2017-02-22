eb00e0b67aa2c9e1d73633634d299e3f8a5b1d63
samer-- <s.abdallah@ucl.ac.uk>
2017-02-22 12:04:25 +0000

    Max likelihood and Max a posterior both working nicely

--- cclab/prolog/cctab/ccptab1.pl
+++ cclab/prolog/cctab/ccptab1.pl
@@ -1,16 +1,14 @@
 :- module(cctab, [ run_tabled/3, cctabled/2, dist/2, (:=)/2
-                 , tables_graph/2
-                 , graph_params/2, graph_pgraph/4
-                 , graph_viterbi/3, graph_inside/3
-                 , graph_stats/5 
-                 , pgraph_tree/4, tree_to_tree/2
-                 , em/5
+                 , tables_graph/2, graph_params/3 
+                 , graph_viterbi/3, graph_inside/3, graph_stats/5 
+                 , pgraph_tree/4, print_tree/1
+                 , em/5, emap/6
                  ]).
 /** <module> Continuation based probabilistic inference.
  TODO: handle more complex top goal by finding all explanations for it.
 */
-:- use_module(library(clpr)).
 :- use_module(library(rbtrees)).
+:- use_module(library(apply_macros)).
 :- use_module(library(typedef)).
 :- use_module(library(dcg_pair)).
 :- use_module(library(math),     [stoch/3]).
@@ -19,6 +17,7 @@
 :- use_module(library(callutils), [mr/5, (*)/4]).
 :- use_module(library(listutils), [rep/3]).
 :- use_module(library(data/pair), [pair/3, snd/2, fst/2, fsnd/3]).
+:- use_module(library(data/tree), []).
 :- use_module(library(delimcc), [p_reset/3, p_shift/2]).
 :- use_module(library(ccstate), [run_nb_state/3, set/1, get/1, app/2, run_state/4]).
 :- use_module(library(lambda2)).
@@ -33,6 +32,7 @@
 :- type values == list(ground).
 :- type graph  == list(pair(goal, list(list(factor)))). % could be map(goal,list(_))
 
+true(_).
 head_to_variant(Head, Variant) :-
    copy_term_nat(Head, Variant),
    numbervars(Variant, 0, _).
@@ -96,9 +96,11 @@ tabled_solution(Tabs, Goal, Expls1) :-
    Goal=Goal0,
    sort(Expls,Expls1).
 
-graph_params(G,Params) :- setof(L, graph_sw(G,L), SWs), maplist(sw_param,SWs,Params).
-graph_sw(G,SW)         :- member(_-Es, G), member(E, Es), member(SW->_, E).
-sw_param(SW,SW-Params) :- call(SW,_,Values,[]), length(Values,N), P is 1/N, rep(N,P,Params).
+graph_params(Spec,G,Params) :- setof(L, graph_sw(G,L), SWs), maplist(sw_init(Spec),SWs,Params).
+graph_sw(G,SW) :- member(_-Es, G), member(E, Es), member(SW->_, E).
+
+sw_init(uniform,SW,SW-Params) :- call(SW,_,Vals,[]), length(Vals,N), P is 1/N, rep(N,P,Params).
+sw_init(K*Spec,SW,SW-Params) :- sw_init(Spec,SW,SW-P0), maplist(mul(K), P0, Params).
 
 % --------- graphs with probabilities -----------
 :- type p_graph == list(p_soln).
@@ -109,44 +111,41 @@ pmap(X,Y,M1,M2) :- rb_insert_new(M1,X,Y,M2), !.
 pmap(X,Y,M,M) :- rb_lookup(X,Y,M).
 
 pmap_sw(Map,SW) :- rb_in(SW->_,_,Map).
-collate_sw(Lookup,SW,SW-Info) :- call(SW,_,Vals,[]), maplist(call(Lookup,SW),Vals,Info).
+pmap_sw_collate(Map,Def,SW,SW-Info) :- call(SW,_,Vals,[]), maplist(pmap_sw_val_p(Map,Def,SW),Vals,Info).
+pmap_sw_val_p(Map,Def,SW,Val,P) :- rb_lookup(SW->Val, P, Map) -> true; call(Def,P).
 
 % inside and viterbi probs
-graph_inside(G, P, PGraph)    :- graph_pgraph(add,G,PGraph,M), pmap_params(M,P).
-graph_viterbi(G, P, PGraph)   :- graph_pgraph(max,G,PGraph,M), pmap_params(M,P).
-graph_pgraph(Agg, G, PG, Map) :- rb_empty(E), foldl(p_soln(Agg), G, PG, E, Map).
-
-pmap_params(Map, Params) :-
+graph_inside(Graph, Params, PGraph)  :- graph_pgraph(add,Graph,Params,PGraph).
+graph_viterbi(Graph, Params, PGraph) :- graph_pgraph(max,Graph,Params,PGraph).
+graph_pgraph(Op, Graph, Params, PGraph) :- 
+   rb_empty(E), 
+   foldl(p_soln(Op), Graph, PGraph, E, Map), 
    setof(SW, pmap_sw(Map,SW), SWs), 
-   maplist(collate_sw(pmap_sw_val_prob(Map)),SWs,Params).
+   maplist(pmap_sw_collate(Map,true),SWs,Params).
 
-p_soln(Agg, Goal-Expls, soln(Goal, Pin, Expls1)) -->
+p_soln(Op, Goal-Expls, soln(Goal, Pin, Expls1)) -->
    pmap(Goal,Pin),
-   run_right(foldl(p_expl(Agg), Expls, Expls1), 0, Pin).
+   run_right(foldl(p_expl(Op), Expls, Expls1), 0, Pin).
 
-p_expl(Agg, Expl, Expl1-Pe) --> run_right(foldl(p_factor, Expl, Expl1), 1, Pe) <\> call(Agg,Pe). 
+p_expl(Op, Expl, Expl1-Pe) --> run_right(foldl(p_factor, Expl, Expl1), 1, Pe) <\> call(Op,Pe). 
 p_factor(M:Atom, (M:Atom)-P) --> pmap(M:Atom,P) <\> mul(P).
 p_factor(SW->Val, (SW->Val)-P) --> pmap(SW->Val, P) <\> mul(P).
 p_factor(@P, const-P) --> \> mul(P).
 
-pmap_sw_val_prob(Map,SW,Val,Prob) :- rb_lookup(SW->Val,Prob,Map) -> true; true. % unify if present
 % For VB: instead of P (=Pr(RV=X_i)), use exp(psi(Alpha_i) - psi(sum_i Alpha_i))
 
 % --------- outside probabilities, ESS ----------------
 :- meta_predicate graph_stats(+,0,?,-,-).
 graph_stats(Graph,Goal,Params,LogProb,Eta) :-
    graph_inside(Graph, Params, InsideG),
-   invert_graph(InsideG, InvGraph),
    memberchk(soln(Goal,Pin,_),InsideG),
    freeze(Pin, LogProb is log(Pin)),
+   foldl(soln_edges,InsideG,QCs,[]), 
+   keysort(QCs,SortedQCs),
+   group_pairs_by_key(SortedQCs, InvGraph),
    rb_empty(Empty), pmap(Goal,1/Pin,Empty, Out1),
    foldl(q_alpha, InvGraph, Out1, Out2),
-   maplist(collate_sw(pmap_sw_val_alpha(Out2))*fst, Params, Eta).
-
-invert_graph(IGraph, InvGraph) :- 
-   foldl(soln_edges,IGraph,QCs,[]), 
-   keysort(QCs,SortedQCs),
-   group_pairs_by_key(SortedQCs, InvGraph).
+   maplist(pmap_sw_collate(Out2,=(0))*fst, Params, Eta).
 
 soln_edges(soln(P,_,Expls)) --> foldl(expl_edges(P),Expls).
 expl_edges(P,Expl-Pe)       --> foldl(factor_edge(Pe,P),Expl).
@@ -155,20 +154,24 @@ factor_edge(Pe,P,Q-BetaQ)   --> [Q-qc(Pe/BetaQ,P)].
 q_alpha(Q-QCs) --> pmap(Q, AlphaQ), run_right(foldl(qc_alpha, QCs), 0, AlphaQ).
 qc_alpha(qc(Pc,P)) --> {mul(AlphaP, Pc, AlphaQC)}, pmap(P, AlphaP) <\> add(AlphaQC).
 
-pmap_sw_val_alpha(Map,SW,Val,Alpha) :- rb_lookup(SW->Val, Alpha, Map), !.
-pmap_sw_val_alpha(_,_,_,0). 
+:- meta_predicate em(+,0,-,+,-), emap(+,+,0,-,+,-).
+em(Graph, Goal, LPs) --> em(maplist(sw_ml), Graph, Goal, LPs).
+emap(Prior, Graph, Goal, LPs) --> em(maplist(sw_map,Prior), Graph, Goal, LPs).
 
-:- meta_predicate em(+,0,-,+,-).
-em(Graph, Goal, LPs, P0, P2) :-
-   graph_stats(Graph, Goal, P, LP, Eta),
-   maplist(update_sw,Eta,P,P1),
-   seqmap_with_progress(1,emstep(t(P,P1,LP)),LPs,P0,P2).
+em(MStep, Graph, Goal, LPs) -->
+   {graph_stats(Graph, Goal, P1, LP, Eta), call(MStep, Eta, P1, P2)},
+   seqmap_with_progress(1,unify3(t(P1,P2,LP)), LPs).
 
-update_sw(SW-Alphas,SW-Probs1,SW-Probs2) :- 
+unify3(PStats,LP,P1,P2) :- copy_term(PStats, t(P1,P2,LP)).
+
+sw_ml(SW-Alphas,SW-Probs1,SW-Probs2) :- 
    maplist(mul,Alphas,Probs1,Counts),
    stoch(Counts,Probs2).
 
-emstep(PStats,LP,P1,P2) :- copy_term(PStats, t(P1,P2,LP)).
+sw_map(SW-Prior,SW-Alphas,SW-Probs1,SW-Probs2) :- 
+   maplist(mul,Alphas,Probs1,Counts),
+   maplist(add,Counts,Prior,Posterior),
+   stoch(Posterior,Probs2).
 
 % ---------- explanation tree with log probability ------
 :- meta_predicate pgraph_tree(+,0,-,-).
@@ -183,8 +186,10 @@ subexpl_tree(G, (M:Subgoal)-_, Subtree-LogProb) :- !, pgraph_tree(G, M:Subgoal, 
 subexpl_tree(_, L-Pin, L-LP) :- LP is log(Pin).
 
 % ---- tree conversion and printing ----
+print_tree(T) :- tree_to_tree(T,T1), tree:print_tree(T1).
+
 tree_to_tree(@P, node(p(P),[])).
-tree_to_tree(SW->Val, node(t(SW->Val),[])).
+tree_to_tree((_:SW)->Val, node(t(SW->Val),[])).
 tree_to_tree(_:Head :- Expls, node(nt(Label), Subnodes)) :-
    functor(Head,Label,_),
    exclude(=(x), Expls, Expls1),
