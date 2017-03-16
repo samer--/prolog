2b8a00c17ac855bafe4bbf08472577fb070beea2
samer-- <s.abdallah@ucl.ac.uk>
2017-03-16 16:18:47 +0000

    Add log_sum_exp/3 and lazy/4

--- cclab/prolog/cctab/lazymath.pl
+++ cclab/prolog/cctab/lazymath.pl
@@ -1,11 +1,21 @@
-:- module(lazymath, [add/3, mul/3, max/3, stoch/2, log_e/2]).
+:- module(lazymath, [add/3, mul/3, max/3, stoch/2, log_e/2, surp/2, lazy/4, lse/3]).
 
 :- use_module(library(math), [stoch/3]).
 :- use_module(library(insist)).
 
 % lazy arithmetic predicates
-max(X,Y,Z) :- when(ground(X-Y),Z is max(X,Y)).
-add(X,Y,Z) :- when(ground(X-Y),Z is X+Y). %{Z=X+Y}.
-mul(X,Y,Z) :- when(ground(X-Y),Z is X*Y). %{Z=X*Y}.
-stoch(X,Y) :- when(ground(X), insist(stoch(X,Y,_))).
-log_e(X,Y) :- when(ground(X), Y is log(X)).
+max(X,Y,Z) :- when(ground(X-Y), Z is max(X,Y)).
+add(X,Y,Z) :- when(ground(X-Y), Z is X+Y). %{Z=X+Y}.
+mul(X,Y,Z) :- when(ground(X-Y), Z is X*Y). %{Z=X*Y}.
+stoch(X,Y) :- when(ground(X),   insist(stoch(X,Y,_))).
+log_e(X,Y) :- when(ground(X),   Y is log(X)).
+surp(P,LP) :- when(ground(P),   LP is -log(P)).
+lse(X,Y,Z) :- when(ground(X-Y), log_sum_exp(X,Y,Z)).
+
+log_sum_exp(-inf,Y,Y) :- !.
+log_sum_exp(X,-inf,X) :- !.
+log_sum_exp(X,Y,Z) :- M is max(X,Y), Z is M + log(exp(X-M) + exp(Y-M)).
+
+lazy(P,X,Y,Z) :- freeze(Z,call(P,X,Y,Z)).
+user:goal_expansion(lazy(P,X,Y,Z), freeze(Z,call(P,X,Y,Z))).
+
