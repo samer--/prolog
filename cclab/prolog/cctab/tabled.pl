96ec84b2676ab19e04801b4da76051a92199a30e
samer-- <s.abdallah@ucl.ac.uk>
2017-03-17 16:40:54 +0000

    Add small grammar from ptabled.pl

--- cclab/prolog/cctab/tabled.pl
+++ cclab/prolog/cctab/tabled.pl
@@ -31,9 +31,9 @@ pathl1 --> pathl1, edge; edge.
 pathr  --> edge; edge, pathr.
 pathr1 --> edge, pathr1; edge.
 
-:- cctable pp//0, qq//0.
-pp --> qq, edge; edge.
-qq --> pp.
+:- cctable ppp//0, qqq//0.
+ppp --> qqq, edge; edge.
+qqq --> ppp.
 
 % for testing handling of input and output variables in continuations.
 path_a(Y) :- pathl(a,X), Y=a(X).
@@ -46,4 +46,46 @@ sent --> {member(A, [b,l,r])}, sent1(A).
 sent1(b) --> {member(W,[cool,wicked])}, [W].
 sent1(l) --> sent, [not].
 sent1(r) --> [really], sent.
+
+:- cctable last/2.
+last([X],X).
+last([_|Xs],X) :- last(Xs,X).
+
+
+% specify preterminals as DCG that spits out alternatives
+user:term_expansion(Lab | Body, Clause) :-
+   dcg_translate_rule(Lab --> Body, Clause).
+
+:- meta_predicate +(2,?,?).
++Lab --> [T], {call(Lab,Vals,[]), member(T,Vals)}.
+
+:- cctable s//0, np//0, vp//0, pp//0, nom//0.
+
+s --> np, vp.
+
+np --> +d, nom
+     ; +pn
+     ; np, pp.
+
+vp --> +iv
+     ; +tv, np
+     ; +dv, np, np
+     ; vp, pp
+     ; +mv, s.
+
+nom --> +n
+      ; +adj, nom.
+
+pp --> +p, np.
+
+% preterminal switch declarations
+adj | [hot,cold,thin,fat,disgusting,lovely].
+pn  | [alice, bob, cuthbert, delia, edna].
+d   | [the,a,some,my]. % ,every,no].
+mv  | [knew,thought,believed,said].
+dv  | [gave,made,baked].
+tv  | [saw, ate, hated, baked, liked, walked, ran, loved, caught].
+iv  | [lived, worked].
+n   | [dog,telescope,man,cat,mat,cake,box,floor,face,pie,moose,pyjamas,park].
+p   | [with,on,under,in,without,by].
 
