4e43e9e25a8b44573017121b913580ede927c67d
samer-- <s.abdallah@ucl.ac.uk>
2017-04-04 11:55:35 +0100

    Add rb_upd_or_ins/4

--- cclab/prolog/rbutils.pl
+++ cclab/prolog/rbutils.pl
@@ -1,4 +1,4 @@
-:- module(rbutils, [rb_fold/4, rb_get/4, rb_gen/3, rb_trans/5, rb_app/4, rb_add/4, rb_app_or_new/5]).
+:- module(rbutils, [rb_fold/4, rb_get/4, rb_gen/3, rb_trans/5, rb_app/4, rb_add/4, rb_upd_or_ins/4, rb_app_or_new/5]).
 :- use_module(library(rbtrees)).
 :- reexport(library(rbtrees)).
 
@@ -11,6 +11,8 @@ rb_get(K,V,T,T) :- rb_lookup(K,V,T).
 rb_gen(K,V,T) :- rb_in(K0,V,T), K=K0.
 % rb_app_or_new(K,P,Q,T1,T2) :- rb_apply_or_create(T1,K,P,Q,T2).
 rb_app_or_new(K,P,Q) --> (rb_add(K,V) -> {call(Q,V)}; rb_app(K,P)).
+rb_upd_or_ins(K,insert(V)) --> rb_add(K,V), !.
+rb_upd_or_ins(K,update(V1,V2)) --> rb_trans(K,V1,V2).
 
 user:goal_expansion(rb_add(K,V,T1,T2),rb_insert_new(T1,K,V,T2)).
 user:goal_expansion(rb_trans(K,V1,V2,T1,T2),rb_update(T1,K,V1,V2,T2)).
