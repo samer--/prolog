758cebbb527c70639c8c47555aea63ab49926926
samer-- <s.abdallah@ucl.ac.uk>
2017-04-06 20:45:47 +0100

    Add drop and subsample transducers

--- cclab/prolog/machines.pl
+++ cclab/prolog/machines.pl
@@ -1,5 +1,5 @@
-:- module(machines, [ iterate/4, unfold/2, unfold_finite/2, scanner/3, scan0/4
-                    , (>>)/3, iterator/3, unfolder/3, mapper/3, moore/5, progress/2]).
+:- module(machines, [ iterate/4, unfold/2, unfold_finite/2, scanner/3, scan0/4, (**)//2
+                    , (>>)/3, iterator/3, unfolder/3, mapper/3, moore/5, progress/2, subsample/3, drop/3]).
 
 :- use_module(library(dcg_progress), [seqmap_with_progress//3]).
 :- use_module(library(lazy),         [lazy_unfold/4, lazy_unfold_finite/4]).
@@ -38,6 +38,15 @@ moore_step(TA,TB,OB, Out, SA1-SB1, SA2-SB2) :- call(TA,OA,SA1,SA2), call(TB,OA,S
 iterator(Setup, S0, unfolder(machines:it_step(Step),S0)) :- call(Setup,Step).
 it_step(Step,X-S1,S1,S2) :- call(Step,X,S1,S2).
 
+%% drop(+N:natural +G:unfolder(X,S), -T:unfolder(X,S)) is det.
+drop(N, unfolder(T,S0), unfolder(T,S1)) :- length(X,N), foldl(T,X,S0,S1).
+
+subsample(N, unfolder(T,S0), unfolder(skip(N,T),S0)).
+skip(N,T,X,S1,S2) :- length([X|Y],N), foldl(T,[X|Y],S1,S2).
+
 progress(M1,M2) :- moore(prog_step,snd,0-_,M1,M2).
 prog_step(X,I-_,J-X) :- J is I+1, format('~d: ~w\n',[J,X]).
 
+**(G,N) --> {var(N)} -> rep_var(N,G); rep_nonvar(N,G).
+rep_nonvar(N,G) --> {N=<0} -> []; {M is N-1}, call_dcg(G), rep_nonvar(M,G).
+rep_var(N,G) --> {N=0}; rep_var(M,G), call_dcg(G), {N is M+1}.
