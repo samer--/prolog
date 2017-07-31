1038104b176ba8af06f0bf17ae2f73ea5cafd4f0
samer-- <samer.abdallah.00@gmail.com>
2017-07-31 10:53:53 +0100

    Add new state operators toccnbenv

--- genutils/prolog/ccnbenv.pl
+++ genutils/prolog/ccnbenv.pl
@@ -1,4 +1,4 @@
-:- module(ccnbenv, [ run_nb_env/1, nb_get/2, nb_app/2, nb_app_or_new/3, nb_dump/1 ]).
+:- module(ccnbenv, [ run_nb_env/1, nb_get/2, nb_app/2, nb_new/2, nb_get_or_new/3, nb_app_or_new/3, nb_dump/1 ]).
 
 :- use_module(library(delimcc), [p_reset/3, p_shift/2]).
 :- use_module(library(rbutils)).
@@ -20,6 +20,12 @@ cont(susp(P,Cont),R0) :- nb_getval(R0,M), call(P,R0,M), run(Cont,R0).
 dump(Map,_,M) :- rb_map(M,nb_getval,Map).
 get(K,X,_,M) :- rb_lookup(K,R,M), nb_getval(R,X).
 app(K,P,_,M) :- rb_lookup(K,R,M), nb_getval(R,X), call(P,X,Y), nb_setval(R,Y).
+new(K,New,R0,M1) :- rb_add(K,R,M1,M2), gensym(nbenv,R), call(New,X), nb_setval(R0,M2), nb_setval(R,X).
+get_or_new(K,Read,New,R0,M1) :-
+   rb_upd_or_ins(K,What,M1,M2),
+   (  What=update(R,R) -> nb_getval(R,X),  call(Read,X)
+   ;  What=insert(R)   -> gensym(nbenv,R), call(New,X), nb_setval(R0,M2), nb_setval(R,X)
+   ).
 app_or_new(K,Upd,New,R0,M1) :-
    rb_upd_or_ins(K,What,M1,M2),
    (  What=update(R,R) -> nb_getval(R,X),  call(Upd,X,Y), nb_setval(R,Y)
@@ -29,5 +35,7 @@ app_or_new(K,Upd,New,R0,M1) :-
 :- meta_predicate nb_app(+,2), nb_app_or_new(+,2,1).
 nb_get(K,X) :- p_shift(nbenv, get(K,X)).
 nb_app(K,P) :- p_shift(nbenv, app(K,P)).
+nb_new(K,P) :- p_shift(nbenv, new(K,P)).
 nb_app_or_new(K,P,Q) :- p_shift(nbenv, app_or_new(K,P,Q)).
+nb_get_or_new(K,P,Q) :- p_shift(nbenv, get_or_new(K,P,Q)).
 nb_dump(M) :- p_shift(nbenv, dump(M)).
