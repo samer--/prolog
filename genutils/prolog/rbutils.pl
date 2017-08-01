3f23d3428078ab800277f7dc40a8846fe86419d8
samer-jukedeck <samer@jukedeck.com>
2017-08-01 17:36:24 +0100

    Swap order in upd or ins: try upd first

--- genutils/prolog/rbutils.pl
+++ genutils/prolog/rbutils.pl
@@ -10,8 +10,8 @@ rb_add(K,V,T1,T2) :- rb_insert_new(T1,K,V,T2).
 rb_get(K,V,T,T) :- rb_lookup(K,V,T).
 % rb_app_or_new(K,P,Q,T1,T2) :- rb_apply_or_create(T1,K,P,Q,T2).
 rb_app_or_new(K,P,Q) --> (rb_add(K,V) -> {call(Q,V)}; rb_app(K,P)).
-rb_upd_or_ins(K,insert(V)) --> rb_add(K,V), !.
-rb_upd_or_ins(K,update(V1,V2)) --> rb_upd(K,V1,V2).
+rb_upd_or_ins(K,update(V1,V2)) --> rb_upd(K,V1,V2), !.
+rb_upd_or_ins(K,insert(V)) --> rb_add(K,V).
 
 user:goal_expansion(rb_add(K,V,T1,T2),rb_insert_new(T1,K,V,T2)).
 user:goal_expansion(rb_upd(K,V1,V2,T1,T2),rb_update(T1,K,V1,V2,T2)).
