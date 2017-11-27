e73c05fa51842ea8927d5127f393338719e29dbf
samer-- <samer.abdallah.00@gmail.com>
2017-11-27 11:15:52 +0000

    Expore ref_maybe_app_ref/2 and add docstrings

--- genutils/prolog/ccref.pl
+++ genutils/prolog/ccref.pl
@@ -4,7 +4,7 @@
 :- use_module(library(ccstate), [run_state/4, app/2]).
 :- use_module(library(data/store)).
 
-:- meta_predicate run_ref(0), ref_app(+,2), ref_app_ref(+,2).
+:- meta_predicate run_ref(0), ref_app(+,2), ref_app_ref(+,2), ref_maybe_app_ref(+,2).
 
 %% run_ref(+P:pred) is det.
 %  Run P inside a run_state/4 with the prompt set to =|ref|=, providing
@@ -18,6 +18,10 @@ ref_get(R,X) :- app(ref, store_get(R,X)).
 ref_set(R,X) :- app(ref, store_set(R,X)).
 ref_app(R,P) :- app(ref, store_apply(R,P)).
 ref_upd(R,X,Y) :- app(ref, store_upd(R,X,Y)).
+
+%! ref_app_ref(+R:ref(A), +P:pred(+A,-P)) is det.
+%  Use P to update contents of R, like ref_app/2, but with
+%  references still available as a computational effect.
 ref_app_ref(R,P) :- app(ref, lifted_app(R,P)).
 
 lifted_app(R,P) -->
@@ -33,4 +37,4 @@ ref_maybe_app_ref(R,P) :- app(ref, lifted_app_maybe(R,P)).
 lifted_app_maybe(R,P) -->
    store_get(R,X1),
    run_state(ref, call(P,X1,MX2)),
-   ({MX2 = just(X2)} -> store_set(R,X2); [])
+   ({MX2 = just(X2)} -> store_set(R,X2); []).
