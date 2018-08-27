0c8cba6ac29cdd4e55bd15756a9278c3a8f93ce7
samer-- <samer.abdallah.00@gmail.com>
2018-08-27 20:31:33 +0100

    Move lazy/4 here from ccprism lazymath

--- genutils/prolog/lazy.pl
+++ genutils/prolog/lazy.pl
@@ -8,9 +8,10 @@
    ,  lazy_maplist/3
    ,  lazy_maplist/4
    ,  lazy_repeat/2
+   ,  lazy/4
 	]).
 
-:- meta_predicate 
+:- meta_predicate
 		lazy_seqmap(4,?,?,?,?)
    ,	lazy_seqmap(3,?,?,?)
 	,	lazy_unfold(4,?,?,?,?)
@@ -19,25 +20,32 @@
    ,	lazy_maplist(1,?)
    ,	lazy_maplist(2,?,?)
    ,	lazy_maplist(3,?,?,?)
+   ,  lazy(3,?,?,-)
 	.
+
+%% lazy(+P:pred(+A,+B,-C), X:A, Y:B, -Z:C) is det
+%  Lazy version of call/4, triggered when Z is needed. Goal
+%  expansion version allows macro expansion of call(P, ...).
+lazy(P,X,Y,Z) :- freeze(Z,call(P,X,Y,Z)).
+user:goal_expansion(lazy(P,X,Y,Z), freeze(Z,call(P,X,Y,Z))).
 
 %% lazy_unfold( +P:pred(-A1,-A2,+S,-S), -XX1:list(A1), -XX2:list(A2), +S1:S, -S2:S) is det.
 %% lazy_unfold( +P:pred(-A1,+S,-S), -XX1:list(A1), +S1:S, -S2:S) is det.
 %
 %  Lazily unfold an infinite stream
 lazy_unfold(P,[X1|XX],[Y1|YY],S1,S3) :-
-	call(P,X1,Y1,S1,S2), 
+	call(P,X1,Y1,S1,S2),
 	freeze(YY,lazy:lazy_unfold(P,XX,YY,S2,S3)).
 
 lazy_unfold(P,[X1|XX],S1,S3) :-
-	call(P,X1,S1,S2), 
+	call(P,X1,S1,S2),
 	freeze(XX,lazy:lazy_unfold(P,XX,S2,S3)).
 
 %% lazy_unfold_finite( +P:pred(-A1,+S,-S), -XX1:list(A1), +S1:S, -S2:S) is det.
 %  Lazily unfold a finite list or infinite stream. If unfolding predicate fails,
 %  then a finite list is produced.
 lazy_unfold_finite(P,Xs,S1,S3) :-
-   (  call(P,X1,S1,S2) 
+   (  call(P,X1,S1,S2)
    -> Xs=[X1|XX], freeze(XX,lazy:lazy_unfold_finite(P,XX,S2,S3))
    ;  Xs=[]
    ).
@@ -45,17 +53,17 @@ lazy_unfold_finite(P,Xs,S1,S3) :-
 %% lazy_seqmap( +P:pred(A1,A2,S,S), +XX1:list(A1), -XX2:list(A2), S1:S, S2:S) is det.
 %% lazy_seqmap( +P:pred(A1,A2,S,S), -XX1:list(A1), -XX2:list(A2), S1:S, S2:S) is nondet.
 %% lazy_seqmap( +P:pred(A1,S,S), -XX1:list(A1), S1:S, S2:S) is nondet.
-%  
-%  Lazy versions of dcg_core:seqmap//{2,3} - can succeed for any lenght of list. 
+%
+%  Lazy versions of dcg_core:seqmap//{2,3} - can succeed for any lenght of list.
 %  Computation is frozen on last list, which is to be understood as an 'output'.
 lazy_seqmap(_,[],[],S,S).
 lazy_seqmap(P,[X1|XX],[Y1|YY],S1,S3) :-
-	call(P,X1,Y1,S1,S2), 
+	call(P,X1,Y1,S1,S2),
 	freeze(YY,lazy:lazy_seqmap(P,XX,YY,S2,S3)).
 
 lazy_seqmap(_,[],S,S).
 lazy_seqmap(P,[X1|XX],S1,S3) :-
-	call(P,X1,S1,S2), 
+	call(P,X1,S1,S2),
 	freeze(XX,lazy:lazy_seqmap(P,XX,S2,S3)).
 
 lazy_maplist(P,[X|XX]) :- call(P,X), freeze(XX,lazy:lazy_maplist(P,XX)).
