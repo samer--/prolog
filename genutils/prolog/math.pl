222136ebe45481ecb03057faa3319b8b212cb619
samer-- <samer.abdallah.00@gmail.com>
2018-08-27 20:21:04 +0100

    Move map_sum from ccprism

--- genutils/prolog/math.pl
+++ genutils/prolog/math.pl
@@ -1,19 +1,5 @@
-:- module(math,
-	[	mul/3
-	,	sub/3
-	,	add/3
-	,	neg/2
-	,	divby/3
-	,	equal/3
-	,	prodlist/2
-	,	recip/2
-	,	stoch/3
-	,	to_float/2
-   ,  max/3
-   ,  gammaln/2
-   ,  log/2
-   ,  exp/2
-	]).
+:- module(math, [mul/3, sub/3, add/3, neg/2, divby/3, recip/2, log/2, exp/2, gammaln/2,
+                 max/3, equal/3, stoch/3, to_float/2, prodlist/2, map_sum/3, map_sum/4]).
 
 :- use_module(library(apply_macros)).
 
@@ -45,3 +31,15 @@ stoch(H,P,N) :- sumlist(H,N), N>0, maplist(divby(N),H,P).
 %  Return the floating point value of Expr as evaluated
 %  by is/2.
 to_float(X,Y) :- Y is float(X).
+
+%% map_sum(+P:pred(+number,+number,-number), +L1:list(number), +L2:list(number), -S:number) is det.
+%% map_sum(+P:pred(+number,-number), +L:list(number), -S:number) is det.
+%
+%  Map P (arity N) over (N-1) supplied lists sum the resulting (Nth) list.
+%  Provided as predicates and goal expansions.
+:- meta_predicate map_sum(2,+,-), map_sum(3,+,+,-).
+map_sum(P,X,Y,Sum) :- maplist(P,X,Y,Z), sum_list(Z,Sum).
+map_sum(P,X,Sum)   :- maplist(P,X,Y),   sum_list(Y,Sum).
+
+user:goal_expansion(map_sum(P,X,Sum),   (maplist(P,X,Y),   sum_list(Y,Sum))).
+user:goal_expansion(map_sum(P,X,Y,Sum), (maplist(P,X,Y,Z), sum_list(Z,Sum))).
